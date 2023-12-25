b28e7b8d747951beffcb6c7f757a1f28a30008c1
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-25 15:25:22 +0530

    chore: add base model with timestamp and UUID as primary key fields

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import uuid
+from django.db.models import Model, UUIDField, DateTimeField
 
-# Create your models here.
+
+class BaseModel(Model):
+    """
+    An abstract base class model that provides UUID as the primary key and
+    self-managed created_at and updated_at timestamp fields.
+    """
+
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
