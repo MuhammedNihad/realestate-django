605c91145db29c85e6da184762fbbc530a52df26
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-25 10:58:59 +0530

    chore: switch database from SQLite to PostgreSQL Update database configuration and settings to use PostgreSQL instead of default SQLite database.

--- realestate/settings.py
+++ realestate/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Parse database connection url strings
+# like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    'default': env.db(),
 }
 
 
