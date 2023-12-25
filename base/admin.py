c7a1830cbabf089cba2ace0e70124b585476a724
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-25 16:34:48 +0530

    chore: add base admin class
    Add BaseAdmin class to hold common configurations or funcitionalities for model admins.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class BaseAdmin(admin.ModelAdmin):
+    """
+    A base admin class that provides common configurations for model admins.
+
+    Attributes:
+        list_filter (list): A list of fields that can be used to filter the model list view.
+        readonly_fields (list): A list of fields that are displayed as read-only in the admin interface.
+    """
+
+    list_filter = ["created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
