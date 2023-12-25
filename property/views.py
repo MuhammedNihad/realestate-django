5ee0524fd05411cae11ebb3d18f3ae6147a187d1
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-25 20:53:31 +0530

    feat: render home page

--- property/views.py
+++ property/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    """
+    Render page for choosing the type/category of property to post.
+    """
+
+    template_name = "index.html"
+
+
+home_page_view = HomePageView.as_view()
