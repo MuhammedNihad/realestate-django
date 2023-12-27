06bb52daa93d2a8c7647751e9ef4f8daa1f07899
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-27 09:55:27 +0530

    feat: add style to unit dropdown in searchbar and update alignment

--- property/forms.py
+++ property/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, CharField, ChoiceField, Select
+from django.forms import Form, CharField, ChoiceField
 from .models import Unit
 
 from crispy_forms.helper import FormHelper
@@ -8,7 +8,6 @@ from crispy_forms.layout import Layout, Submit, Field
 class PropertySearchForm(Form):
     unit = ChoiceField(
         choices=Unit.BedroomType.choices,
-        widget=Select(attrs={"class": "form-select-sm choose-city-dropdown"}),
         required=False,
         label="",
     )
@@ -18,7 +17,7 @@ class PropertySearchForm(Form):
         super(PropertySearchForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Field("unit", css_class="form-select-sm"),
-            Field("search_query", css_class="form-control px-4", placeholder="Search properties..."),
-            Submit("submit", "Search", css_class="btn btn-primary"),
+            Field("unit", css_class="form-select-sm choose-unit-search-dropdown"),
+            Field("search_query", css_class="form-control px-4 h-100", placeholder="Search properties..."),
+            Submit("submit", "Search", css_class="btn btn-primary mb-3"),
         )
